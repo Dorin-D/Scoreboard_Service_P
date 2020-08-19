0e33a78ad6346e21a6530c4d4b9dd4ae164f2c03
Dorin-D <justdorin2011@gmail.com>
2020-08-19 13:48:10 +0300

    Added sorting functionality

--- scoreboard_service.py
+++ scoreboard_service.py
@@ -15,6 +15,37 @@ def add_score(user, score, file_name):
 	file.write(user + " " + str(score))
 	file.close()
 
-#print(read_scores("scores"))
-add_score("Otter_Possum", "13", "scores")
+def partition(list, start, end):
+	pivot = list[start][1]
+	low = start + 1
+	high = end
+	
+	while True:
+		while low <= high and list[high][1] >= pivot:
+			high = high - 1
+		
+		while low <= high and list[low][1] <= pivot:
+			low = low + 1
 
+		if low <= high:
+			list[low][1], list[high][1] = list[high][1], list[low][1]
+		else:
+			break
+			
+	list[start][1], list[high][1] = list[high][1], list[start][1]
+	return high
+	
+def quick_sort(list, start, end):
+	if start >= end:
+		return
+	
+	p = partition(list, start, end)
+	quick_sort(list, start, p-1)
+	quick_sort(list, p+1, end)
+
+#print(read_scores("scores"))
+#add_score("Otter_Possum", "13", "scores")
+
+scoreboard = read_scores("scores")
+quick_sort(scoreboard, 0, len(scoreboard)-1)
+print(scoreboard)
